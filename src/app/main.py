f502b1f058366994a7acd389129dced81543f12c
ankit8github <ankit.kashyap0221@gmail.com>
2026-02-08 02:35:33 +0530

    redploy

--- src/app/main.py
+++ src/app/main.py
@@ -213,9 +213,11 @@ if __name__ == "__main__":
     import uvicorn
     import os
 
+    port = int(os.environ.get("PORT", 7860))
+
     uvicorn.run(
         "src.app.main:app",
-        host="0.0.0.0",   # REQUIRED for Docker
-        port=7860,        # Must match Docker port
+        host="0.0.0.0",
+        port=port,
         reload=False
     )
